053d0d7df75392e12bf2b6a7e2e54b5c340fe33e
Allie Schultz <allie.m.schultz1@gmail.com>
2025-05-09 14:37:14 -0700

    added boolean logic submission assignment

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Car insurance approval program
+
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? (true/false)");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualified for car insurance?");
+            Console.WriteLine(age > 15 && !dui && tickets <= 3);
+            Console.ReadLine();
+
+        }
+    }
+}
