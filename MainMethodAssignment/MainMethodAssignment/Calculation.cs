fd7a9730432860f8f8395dd89dfdde6466d0e542
Allie Schultz <allie.m.schultz1@gmail.com>
2025-05-21 15:48:55 -0700

    Added MainMethodAssignment files

---
+++ MainMethodAssignment/MainMethodAssignment/Calculation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    internal class Calculation
+    {
+        public int Opperation(int num)
+        {
+            return num + num;
+        }
+
+        public double Opperation(decimal num)
+        {
+            decimal intermediateNum = (num / 2);
+            double result = (double)intermediateNum;
+            return result;
+        }
+
+        public int Opperation(string num)
+        {
+            int convertedNum = Convert.ToInt32(num);
+            int result = convertedNum * convertedNum;
+            return result;
+        }
+
+
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 5;
+            Console.WriteLine("We are performing opperations on an int, decimal, and string");
+            Calculation calc = new Calculation();
+
+            // Call the method with an int
+            int result = calc.Opperation(value);
+            Console.WriteLine(value + " + " + value + " = " + result);
+
+            // Call the method with a decimal
+            decimal decimalValue = 10.7m;
+            double resultDecimal = calc.Opperation(decimalValue);
+            Console.WriteLine(decimalValue + " / 2 = " + resultDecimal);
+
+            // Call the method with a string
+            string stringValue = "7";
+            int resultString = calc.Opperation(stringValue);
+            Console.WriteLine(stringValue + " * " + stringValue + " = " + resultString);
+
+            Console.ReadLine();
+        }
+    }
+}
