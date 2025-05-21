3aab638931257fb8c42e3ba7a56d375a7faba81c
Allie Schultz <allie.m.schultz1@gmail.com>
2025-05-21 12:24:11 -0700

    Added Methods Assignment of Math Opperations

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Math.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    public class Math
+    {
+        public int Add(int numAdd)
+        {
+            return numAdd + 50;
+        }
+        public int Subtract(int numSub)
+        {
+            return numSub - 1;
+        }
+        public int Multiply(int numMult)
+        {
+            return numMult * 3;
+        }
+      
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number to perform math operations on:");
+            string input = (Console.ReadLine());
+            int number;
+            if (int.TryParse(input, out number))
+            {
+                Console.WriteLine("You entered: " + number);
+
+                Math math = new Math();
+
+                int addResult = math.Add(number);
+                Console.WriteLine("Addition result: " + addResult);
+
+                int subtractResult = math.Subtract(number);
+                Console.WriteLine("Subtraction result: " + subtractResult);
+
+                int multiplyResult = math.Multiply(number);
+                Console.WriteLine("Multiplication result: " + multiplyResult);
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
