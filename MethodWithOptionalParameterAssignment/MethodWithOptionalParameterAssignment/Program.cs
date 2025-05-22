1117f92f259a891e72becd4d60ec9d7642d00d59
Allie Schultz <allie.m.schultz1@gmail.com>
2025-05-22 10:02:25 -0700

    Added optional method assignment

---
+++ MethodWithOptionalParameterAssignment/MethodWithOptionalParameterAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodWithOptionalParameterAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            twoParameters mult = new twoParameters();
+
+            Console.WriteLine("Please enter an integer:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter another integer (or press Enter to use the default value of 2):");
+            string input = Console.ReadLine();
+            int num2;
+            if (string.IsNullOrEmpty(input))
+            {
+                num2 = 2; // Default value
+            }
+            else
+            {
+                num2 = Convert.ToInt32(input);
+            }
+
+            int result = mult.Multiply(num1, num2);
+            Console.WriteLine(num1 + " * " + num2 + " = " + result );
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodWithOptionalParameterAssignment/MethodWithOptionalParameterAssignment/twoParameters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodWithOptionalParameterAssignment
+{
+    public class twoParameters
+    {
+        public int Multiply(int num1, int num2 = 2)
+        {
+            return num1 * num2;
+        }
+    }
+}
