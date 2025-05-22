751e8698241db2839af32f6a256c0d0845e3a1de
Allie Schultz <allie.m.schultz1@gmail.com>
2025-05-22 10:17:57 -0700

    Added VoidMethodAssignment

---
+++ VoidMethodAssignment/VoidMethodAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            voidMethod vm = new voidMethod();
+
+            int numa = 5;
+            int numb = 10;
+
+            Console.WriteLine("Parameter passed regularly:");
+            vm.twoInts(numa, numb);
+            Console.WriteLine("\nParameter passed by reference:");
+            vm.twoInts(a: numa, b: numb);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ VoidMethodAssignment/VoidMethodAssignment/voidMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssignment
+{
+    public class voidMethod
+    {
+        public void twoInts(int a, int b)
+        {
+           int Mult = a * 3;
+            Console.WriteLine(a + " * 3 = " + Mult);
+            Console.WriteLine("The second integer is: " + b);
+        }
+    }
+}
