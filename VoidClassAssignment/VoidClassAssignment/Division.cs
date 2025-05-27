f3be5a20416ab5474e99523f39d984f4b6714546
Allie Schultz <allie.m.schultz1@gmail.com>
2025-05-27 16:05:13 -0700

    Added void class assignment

---
+++ VoidClassAssignment/VoidClassAssignment/Division.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidClassAssignment
+{
+    public class Division
+    {
+        public void DivideBy2(double number, out  double result)
+        {
+            double num = Convert.ToDouble(number);
+            result = num / 2;
+        }
+
+        public void DivideBy2(int number, out int result)
+        {
+            int num = Convert.ToInt32(number);
+            result = num / 2;
+        }
+    }
+
+    public static class DivisionHelper
+        {
+            public static double DivideBy5(double divisor)
+            {
+                return divisor / 5;
+            }
+        }
+    
+}
---
+++ VoidClassAssignment/VoidClassAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidClassAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Division division = new Division();
+
+            Console.WriteLine("Enter a number to divide by 2: ");
+            string input = Console.ReadLine();
+            int num;
+
+            if (int.TryParse(input, out num))
+            {
+                int result;
+                division.DivideBy2(num, out result);
+                Console.WriteLine($"The result of dividing {num} by 2 is: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+
+            double doubleNum = 36.7;
+            double doubleResult;
+            division.DivideBy2(doubleNum, out doubleResult);
+            Console.WriteLine($"The result of dividing {doubleNum} by 2 is: {doubleResult}");
+
+            double intNum = 100.00;
+            double intResult = DivisionHelper.DivideBy5(intNum);
+            Console.WriteLine("100 / 5 = " + intResult);
+            Console.ReadLine();
+
+      
+
+
+        }
+    }
+}
