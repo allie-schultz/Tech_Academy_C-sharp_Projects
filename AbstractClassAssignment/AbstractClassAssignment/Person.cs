36d2ca4755e6f1e01cdfedd7dd75dba6cc203643
Allie Schultz <allie.m.schultz1@gmail.com>
2025-05-28 16:53:17 -0700

    Added Abstract Class Assignment

---
+++ AbstractClassAssignment/AbstractClassAssignment/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    public abstract class Person //abstract: cannot be insantiated directly, must be inherited from
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public abstract void SayName();
+    }
+}
---
+++ AbstractClassAssignment/AbstractClassAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of Employee
+            Employee employee = new Employee();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+            employee.ID = 1;
+            // Call the SayName method
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
