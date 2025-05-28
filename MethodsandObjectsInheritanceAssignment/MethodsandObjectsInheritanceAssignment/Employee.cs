1b78c69e79e1fad67f528799c8bccd3aadb08c1e
Allie Schultz <allie.m.schultz1@gmail.com>
2025-05-28 13:32:52 -0700

    Added Inheritance Assignment

---
+++ MethodsandObjectsInheritanceAssignment/MethodsandObjectsInheritanceAssignment/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsandObjectsInheritanceAssignment
+{
+    internal class Employee : Person
+    {
+        public int ID { get; set; }
+    }
+}
