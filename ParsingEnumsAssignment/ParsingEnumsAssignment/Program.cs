8604b56c5972fd3e5b207cd4a34b7c5b48eba050
Allie Schultz <allie.m.schultz1@gmail.com>
2025-06-03 13:28:33 -0700

    Added days of the week enum parsing assignment

---
+++ ParsingEnumsAssignment/ParsingEnumsAssignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DaysOfWeek currentDay;
+
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week");
+                string input = Console.ReadLine();
+                currentDay = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), input, true);
+                Console.WriteLine($"Today is {currentDay}.");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            Console.ReadLine();
+        }
+        public enum DaysOfWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+    }
+}
