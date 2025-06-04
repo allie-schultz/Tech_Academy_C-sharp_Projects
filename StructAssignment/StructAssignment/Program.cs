73ba92ff46ec255f192c8e4168ee02eed84cc49a
Allie Schultz <allie.m.schultz1@gmail.com>
2025-06-04 14:13:33 -0700

    Added struct assignment

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num;
+            num.Amount = 123.456m;
+            Console.WriteLine("The amount is: " + num.Amount);
+            Console.ReadLine();
+        }
+        public struct Number
+        {
+            public decimal Amount;
+        }
+    }
+}
